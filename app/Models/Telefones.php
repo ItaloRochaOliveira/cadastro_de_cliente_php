c50ae660527a78fd61efbbc5952616ef62eee043
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2024-01-31 22:22:05 -0300

    feat: add connection with database

---
+++ app/Models/Telefones.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Telefones extends Model
+{
+    use HasFactory;
+}
